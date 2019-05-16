19f784a77ff872000635abf069b065b6a6ac914e
Iconoclast <cxd4@member.fsf.org>
2019-05-16 12:41:58 -0400

    renamed draw_power() to graph_power()

--- graph.js
+++ graph.js
@@ -47,3 +47,54 @@ function main_GL() {
     } while (error_code !== GL_NO_ERROR);
     return;
 }
+
+/**
+ * End of graph definition and initialization procedures.
+ * Begin definitions for graph construction functions.
+ */
+
+function graph_power() {
+    "use strict";
+    var x = -1.0 / inverse_zoom;
+    var i = 0;
+    var coords = 2;
+    var stride = 4 * 4; /* coords_per_vertex * sizeof(GLfloat) */
+    var vertex_buffer = [];
+
+    var c;
+    var n;
+
+    while (i < raster_pitch) {
+        vertex_buffer[4*i + X] = x;
+        x += 2.0 / raster_pitch;
+        vertex_buffer[4*i + Z] = 0.0;
+        vertex_buffer[4*i + W] = inverse_zoom;
+        i += 1;
+    }
+
+    clear_graph();
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+    glLineWidth(1.0);
+
+    c = document.getElementById("c").value;
+    if (!c) {
+        c = 1;
+    }
+    n = document.getElementById("n").value;
+    if (!n) {
+        n = 0;
+    }
+
+    i = 0;
+    x = -1.0 / inverse_zoom;
+    while (i < raster_pitch) {
+        vertex_buffer[4*i + Y] = c * Math.pow(x, n);
+        x += 2.0 / raster_pitch;
+        i += 1;
+    }
+    glColor4f(0, 1, 0, 1); /* green (original function) */
+    glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
+    glDrawArrays(GL_LINE_STRIP, 0, i);
+    return;
+}
--- power.js
+++ power.js
@@ -28,7 +28,7 @@ function tangent_line(x_raster) {
     document.getElementById("m").innerHTML = m;
     document.getElementById("b").innerHTML = b;
 
-    draw_power();
+    graph_power();
     glEnableClientState(GL_VERTEX_ARRAY);
     glDisableClientState(GL_COLOR_ARRAY);
     glLineWidth(1.0);
@@ -36,51 +36,5 @@ function tangent_line(x_raster) {
     glColor4f(1, 0, 0, 1); /* red (tangent line function of derivative) */
     glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
     glDrawArrays(GL_LINES, 0, 2);
-    return;
-}
-
-function draw_power() {
-    "use strict";
-    var x = -1.0 / inverse_zoom;
-    var i = 0;
-    var coords = 2;
-    var stride = 4 * 4; /* coords_per_vertex * sizeof(GLfloat) */
-    var vertex_buffer = [];
-
-    var c;
-    var n;
-
-    while (i < raster_pitch) {
-        vertex_buffer[4*i + X] = x;
-        x += 2.0 / raster_pitch;
-        vertex_buffer[4*i + Z] = 0.0;
-        vertex_buffer[4*i + W] = inverse_zoom;
-        i += 1;
-    }
-
-    clear_graph();
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_COLOR_ARRAY);
-    glLineWidth(1.0);
-
-    c = document.getElementById("c").value;
-    if (!c) {
-        c = 1;
-    }
-    n = document.getElementById("n").value;
-    if (!n) {
-        n = 0;
-    }
-
-    i = 0;
-    x = -1.0 / inverse_zoom;
-    while (i < raster_pitch) {
-        vertex_buffer[4*i + Y] = c * Math.pow(x, n);
-        x += 2.0 / raster_pitch;
-        i += 1;
-    }
-    glColor4f(0, 1, 0, 1); /* green (original function) */
-    glVertexPointer(coords, GL_FLOAT, stride, vertex_buffer);
-    glDrawArrays(GL_LINE_STRIP, 0, i);
     return;
 }
